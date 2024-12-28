cd593278f81080db7df9180883317f99a2952d66
Sam Foreman <saforem2@gmail.com>
2024-12-28 14:07:19 -0600

    feat: Update `config/options.lua`

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,23 +1,15 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
--- <<<<<<< HEAD
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 --
--- vim.g.vim_markdown_auto_insert_bullets = 0
--- vim.g.vim_markdown_new_list_item_indent = 0
--- vim.g.vim_markdown_strikethrough = 1
--- vim.g.tex_superscripts= "[0-9a-zA-W.,:;+-<>/()=]"
--- vim.g.tex_subscripts= "[0-9aehijklmnoprstuvx,+-/().]"
--- =======
 vim.g.vim_markdown_auto_insert_bullets = 0
 vim.g.vim_markdown_new_list_item_indent = 0
 vim.g.vim_markdown_strikethrough = 1
 vim.g.tex_superscripts = "[0-9a-zA-W.,:;+-<>/()=]"
 vim.g.tex_subscripts = "[0-9aehijklmnoprstuvx,+-/().]"
--- >>>>>>> bf7322f419b09204c03b3e20713de928b19a37ea
 vim.g.vim_markdown_frontmatter = 1
 vim.g.vim_markdown_math = 1
 -- vim.g.vim_markdown_fenced_languages = { "csharp=cs", "python=py" }
@@ -25,11 +17,7 @@ vim.g.vim_markdown_folding_level = 2
 -- LazyVim auto format
 vim.g.autoformat = false
 
--- <<<<<<< HEAD
---
--- =======
 vim.g.gitblame_display_virtual_text = 0 -- Disable virtual text
--- >>>>>>> bf7322f419b09204c03b3e20713de928b19a37ea
 
 -- Options for the LazyVim statuscolumn
 -- vim.g.lazyvim_statuscolumn = {
@@ -80,11 +68,7 @@ opt.fillchars = {
 opt.foldlevel = 99
 opt.formatexpr = "v:lua.require'lazyvim.util'.format.formatexpr()"
 opt.formatoptions = "jcroqlnt" -- tcqj
--- <<<<<<< HEAD
 opt.foldexpr = "v:lua.vim.treesitter.foldexpr()" -- Treesitter folding
--- =======
--- opt.foldexpr = "v:lua.vim.treesitter.foldexpr()" -- Treesitter folding
--- >>>>>>> bf7322f419b09204c03b3e20713de928b19a37ea
 opt.foldmethod = "expr" -- Treesitter folding
 opt.grepformat = "%f:%l:%c:%m"
 opt.grepprg = "rg --vimgrep"
