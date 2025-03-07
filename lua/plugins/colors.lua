6842fa0f75d1d4e27200f66b321fa33c9c8edd7b
Sam Foreman <saforem2@gmail.com>
2025-03-07 09:17:42 -0600

    Update `lua/plugins/colors.lua`

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,5 +1,16 @@
 return {
   {
+    'linrongbin16/colorbox.nvim',
+
+    -- don't lazy load
+    lazy = false,
+    -- load with highest priority
+    priority = 1000,
+
+    build = function() require("colorbox").update() end,
+    config = function() require("colorbox").setup() end,
+  },
+  {
     "CodeGradox/onehalf-lush",
     lazy = false,
     enabled = true,
