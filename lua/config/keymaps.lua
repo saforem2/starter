e10b72eead2a255a02b3b1b0539385a351047d5a
Sam Foreman <saforem2@gmail.com>
2025-03-12 10:41:04 -0500

    chore: Update `lua/config/keymaps.lua`

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -66,20 +66,20 @@ map("n", "H", "<Cmd>BufferPrevious<CR>", opts)
 map("n", "L", "<Cmd>BufferNext<CR>", opts)
 
 -- Re-order to previous/next
-map("n", "<A-<>", "<Cmd>BufferMovePrevious<CR>", opts)
-map("n", "<A->>", "<Cmd>BufferMoveNext<CR>", opts)
+-- map("n", "<A-<>", "<Cmd>BufferMovePrevious<CR>", opts)
+-- map("n", "<A->>", "<Cmd>BufferMoveNext<CR>", opts)
 
 -- Goto buffer in position...
-map("n", "<A-1>", "<Cmd>BufferGoto 1<CR>", opts)
-map("n", "<A-2>", "<Cmd>BufferGoto 2<CR>", opts)
-map("n", "<A-3>", "<Cmd>BufferGoto 3<CR>", opts)
-map("n", "<A-4>", "<Cmd>BufferGoto 4<CR>", opts)
-map("n", "<A-5>", "<Cmd>BufferGoto 5<CR>", opts)
-map("n", "<A-6>", "<Cmd>BufferGoto 6<CR>", opts)
-map("n", "<A-7>", "<Cmd>BufferGoto 7<CR>", opts)
-map("n", "<A-8>", "<Cmd>BufferGoto 8<CR>", opts)
-map("n", "<A-9>", "<Cmd>BufferGoto 9<CR>", opts)
-map("n", "<A-0>", "<Cmd>BufferLast<CR>", opts)
+-- map("n", "<A-1>", "<Cmd>BufferGoto 1<CR>", opts)
+-- map("n", "<A-2>", "<Cmd>BufferGoto 2<CR>", opts)
+-- map("n", "<A-3>", "<Cmd>BufferGoto 3<CR>", opts)
+-- map("n", "<A-4>", "<Cmd>BufferGoto 4<CR>", opts)
+-- map("n", "<A-5>", "<Cmd>BufferGoto 5<CR>", opts)
+-- map("n", "<A-6>", "<Cmd>BufferGoto 6<CR>", opts)
+-- map("n", "<A-7>", "<Cmd>BufferGoto 7<CR>", opts)
+-- map("n", "<A-8>", "<Cmd>BufferGoto 8<CR>", opts)
+-- map("n", "<A-9>", "<Cmd>BufferGoto 9<CR>", opts)
+-- map("n", "<A-0>", "<Cmd>BufferLast<CR>", opts)
 
 -- Pin/unpin buffer
 map("n", "<A-p>", "<Cmd>BufferPin<CR>", opts)
@@ -108,7 +108,8 @@ map("n", "<C-s-p>", "<Cmd>BufferPickDelete<CR>", opts)
 -- Sort automatically by...
 map("n", "<Space>bb", "<Cmd>BufferOrderByBufferNumber<CR>", opts)
 map("n", "<Space>bn", "<Cmd>BufferOrderByName<CR>", opts)
-map("n", "<Space>bd", "<Cmd>BufferOrderByDirectory<CR>", opts)
+map("n", "<Space>bD", "<Cmd>BufferOrderByDirectory<CR>", opts)
+map("n", "<Space>bd", "<Cmd>BufferClose<CR>", opts)
 map("n", "<Space>bl", "<Cmd>BufferOrderByLanguage<CR>", opts)
 map("n", "<Space>bw", "<Cmd>BufferOrderByWindowNumber<CR>", opts)
 
@@ -117,32 +118,41 @@ map("n", "<Space>bw", "<Cmd>BufferOrderByWindowNumber<CR>", opts)
 -- :BarbarEnable - enables barbar (enabled by default)
 -- :BarbarDisable - very bad command, should never be used
 
-vim.keymap.set("i", "<C-J>", 'copilot#Accept("\\<CR>")', {
-  expr = true,
-  replace_keycodes = false,
-})
+map("i", "<C-j>", 'copilot#Accept("\\<CR>")', { expr = true, replace_keycodes = false })
+-- vim.keymap.set("i", "<C-J>", 'copilot#Accept("\\<CR>")', {
+--   expr = true,
+--   replace_keycodes = false,
+-- })
 vim.g.copilot_no_tab_map = true
 
-vim.keymap.set(
-  { "v" },
-  "<leader>c",
-  "require('osc52').copy_visual()",
-  { desc = "copy visual selection to system clipboard" }
-)
-vim.keymap.set(
-  { "n" },
-  "<leader>c",
-  "require('osc52').copy_operator()",
-  { desc = "copy visual selection to system clipboard" }
-)
+-- vim.keymap.set(
+--   { "v" },
+--   "<leader>c",
+--   "require('osc52').copy_visual()",
+--   { desc = "copy visual selection to system clipboard" }
+-- )
+-- vim.keymap.set(
+--   { "n" },
+--   "<leader>c",
+--   "require('osc52').copy_operator()",
+--   { desc = "copy visual selection to system clipboard" }
+-- )
+map("v", "<leader>c", "<Plug>(osc52-copy-visual)", { desc = "Copy to system clipboard" })
+map("n", "<leader>c", "<Plug>(osc52-copy-operator)", { desc = "Copy to system clipboard" })
+map("n", "[[", "<Plug>(markdown_go_prev_heading)<CR>", { desc = "Jump to Previous Heading" })
+map("n", "]]", "<Plug>(markdown_go_next_heading)<CR>", { desc = "Jump to Next Heading" })
+map("n", "<leader>\\", "<cmd>QuartoPreview<cr>", { desc = "Quarto Preview" })
+map("n", "0", "^", { desc = "Beginning of line" })
+map("n", "X", [[:keeppatterns substitute/\s*\%#\s*/\r/e <bar> normal! ==<CR>]], { desc = "Split line" })
 
 -- vim.keymap.set({ "n" }, "[v", [[<Plug>Markdown_MoveToNextHeader<CR>]], { desc = "Set cursor to next heading" })
 -- vim.keymap.set({ "n" }, "]v", [[:<Plug>Markdown_MoveToPreviousHeader<CR>]], { desc = "Set cursor to previous heading" })
 -- vim.keymap.set({ "n" }, "[v", "<Plug>(Markdown_MoveToPreviousHeader)<cr>", { desc = "Set cursor to previous heading" })
 -- vim.keymap.set({ "n" }, "]v", "<Plug>(Markdown_MoveToNextHeader)<cr>", { desc = "Set cursor to next heading" })
-vim.keymap.set({ "n" }, "<leader>\\", "<cmd>QuartoPreview<cr>", { desc = "Quarto Preview" })
-vim.keymap.set({ "n" }, "0", "^", { desc = "Beginning of line" })
-vim.keymap.set({ "n" }, "X", [[:keeppatterns substitute/\s*\%#\s*/\r/e <bar> normal! ==<CR>]], { desc = "Split line" })
+--
+-- vim.keymap.set({ "n" }, "<leader>\\", "<cmd>QuartoPreview<cr>", { desc = "Quarto Preview" })
+-- vim.keymap.set({ "n" }, "0", "^", { desc = "Beginning of line" })
+-- vim.keymap.set({ "n" }, "X", [[:keeppatterns substitute/\s*\%#\s*/\r/e <bar> normal! ==<CR>]], { desc = "Split line" })
 --     ["<leader>\\"] = {"<cmd>QuartoPreview<cr>", desc = "Quarto Preview"},
 --     ["0"] = {"^", desc = "Beginning of line"},
 --     ["X"] = {
