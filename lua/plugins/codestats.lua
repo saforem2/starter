0b7a168a47373bd8f96c4313b163fb140d78d743
Sam Foreman <saforem2@gmail.com>
2025-08-23 10:27:08 -0500

    chore: Update config

--- lua/plugins/codestats.lua
+++ lua/plugins/codestats.lua
@@ -1,19 +1,19 @@
 return {
-{
-  'liljaylj/codestats.nvim',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  event = { 'TextChanged', 'InsertEnter' },
-  cmd = { 'CodeStatsXpSend', 'CodeStatsProfileUpdate' },
-  config = function()
-    require('codestats').setup {
-      username = 'samforeman',  -- needed to fetch profile data
-      base_url = 'https://codestats.net',  -- codestats.net base url
-      api_key = os.getenv('CODESTATS_API_KEY') or '',
-      send_on_exit = true,  -- send xp on nvim exit
-      send_on_timer = true,  -- send xp on timer
-      timer_interval = 60000,  -- timer interval in milliseconds (minimum 1000ms to prevent DDoSing codestat.net servers)
-      curl_timeout = 5,  -- curl request timeout in seconds
-    }
-  end,
-},
+  {
+    "liljaylj/codestats.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    event = { "TextChanged", "InsertEnter" },
+    cmd = { "CodeStatsXpSend", "CodeStatsProfileUpdate" },
+    config = function()
+      require("codestats").setup({
+        username = "samforeman", -- needed to fetch profile data
+        base_url = "https://codestats.net", -- codestats.net base url
+        api_key = os.getenv("CODESTATS_API_KEY") or "",
+        send_on_exit = true, -- send xp on nvim exit
+        send_on_timer = true, -- send xp on timer
+        timer_interval = 20000, -- timer interval in milliseconds (minimum 1000ms to prevent DDoSing codestat.net servers)
+        curl_timeout = 5, -- curl request timeout in seconds
+      })
+    end,
+  },
 }
