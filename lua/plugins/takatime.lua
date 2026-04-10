b3157f6651aed9f0adbc37df4e02c21a4e5f38a4
Sam Foreman <saforem2@gmail.com>
2026-04-10 12:40:44 -0500

    chore: Update TakaTime plugin config

--- lua/plugins/takatime.lua
+++ lua/plugins/takatime.lua
@@ -1,11 +1,12 @@
 return {
+    {
   "Rtarun3606k/TakaTime",
-  lazy = true,
-  event = "VeryLazy",
+  lazy = false,
   config = function()
     -- Optional: Enable debug mode if you run into issues
     require("taka-time").setup({
-      debug = false,
+        debug = false
     })
   end,
+},
 }
